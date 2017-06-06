e846c0bd487a1e84b12ad0027e2bbbafe4ac0655
Poling <466680197@qq.com>
2017-06-06 17:02:03 +0800

    d

--- src/renderer/applog.js
+++ src/renderer/applog.js
@@ -12,24 +12,7 @@ const current = moment().format('YYYY-MM-DD');
 if (!existsSync(logFolder)) {
   mkdirp(logFolder);
 }
-console.log(log)
 
 log.transports.file.file = `${logFolder}/log-${current}.txt`;
-
-// log.error('clear log renderer');
-
-/*try {
-  readdirSync(logFolder)
-    .filter(fileName => fileName.includes('.txt'))
-    .forEach((fileName) => {
-      const date = fileName.slice(4, 14);
-      const delFlag = moment().diff(moment(date), 'days') >= 10;
-      if (delFlag) {
-        removeSync(join(logFolder, fileName));
-      }
-    });
-} catch (e) {
-  log.error(e.message);
-}*/
 
 export default { error: log.error, info: log.info };
